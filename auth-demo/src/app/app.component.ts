a896d30f03aec5fb816a1f86c4259f3c3d7f625d
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2025-01-14 11:13:39 +0000

    Implemented profile info display after logge din

--- auth-demo/src/app/app.component.ts
+++ auth-demo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthButtonComponent } from './components/auth-button/auth-button.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AuthButtonComponent],
+  imports: [RouterOutlet, AuthButtonComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ auth-demo/src/app/components/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-profile',
+  imports: [CommonModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+  constructor(public auth: AuthService) { }
+}
